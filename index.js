ebba1f30a9a9385c783dddc7a30cec660eca2d2e
schoi2 <celebril26@gmail.com>
2015-10-28 19:40:32 +0000

    Something descriptive

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log("Hello World!")
+var express = require('express');
+ var app = express();
+
+ app.get('/:club', function (req, res) {
+   res.send('Welcome to the ' + req.params.club + '!');
+ });
+
+ app.get('/:help/', function (req, res)) {
+  res.send('Ctrl + C = stop running' + req.params.club + '!');
+ });
+
+ var server = app.listen(process.env.PORT, function () {
+   var host = server.address().address;
+   var port = server.address().port;
+
+   console.log('Example app listening at http://%s:%s', host, port);
+ });
